1ff7e5effee38818edd9dd74e3cc1e4f1496f5ef
Louisa Goncharenko <93098869+lougoncharenko@users.noreply.github.com>
2024-02-25 12:15:12 -0500

    Add files via upload

---
+++ WhyNotTry/WhyNotTry/WhyNotTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTryApp.swift
+//  WhyNotTry
+//
+//  Created by Louisa Goncharenko on 2/23/24.
+//
+
+import SwiftUI
+
+@main
+struct WhyNotTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
